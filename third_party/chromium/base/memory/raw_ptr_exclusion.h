c7f004ebfacba97698e41b586cc55a210f8485da
kon72 <kinsei0916@gmail.com>
2025-06-14 06:11:14 +0900

    Add checked_iterators

---
+++ third_party/chromium/base/memory/raw_ptr_exclusion.h
@@ -0,0 +1,12 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_MEMORY_RAW_PTR_EXCLUSION_H_
+#define BASE_MEMORY_RAW_PTR_EXCLUSION_H_
+
+#ifndef RAW_PTR_EXCLUSION
+#define RAW_PTR_EXCLUSION
+#endif
+
+#endif  // BASE_MEMORY_RAW_PTR_EXCLUSION_H_
