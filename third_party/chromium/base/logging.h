125072c713ff1d45c5237a6d350e21dadcd818a7
kon72 <kinsei0916@gmail.com>
2025-06-15 02:00:12 +0900

    Add string_util, utf_ostream_operators, utf_string_conversions

---
+++ third_party/chromium/base/logging.h
@@ -0,0 +1,12 @@
+// Copyright 2012 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_LOGGING_H_
+#define BASE_LOGGING_H_
+
+#include "absl/log/absl_log.h"
+
+#define DLOG(severity) ABSL_LOG(severity)
+
+#endif  // BASE_LOGGING_H_
