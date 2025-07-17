75d6ee22fb1447dd3683412419015b277c91f39b
kon72 <kinsei0916@gmail.com>
2025-07-17 18:06:44 +0900

    Fix include directive for platform_thread_handle

--- chromium/base/threading/platform_thread_handle_win.cc
+++ chromium/base/threading/platform_thread_handle_win.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/threading/platform_thread_handle_win.h"
+#include "base/threading/platform_thread_handle.h"
 
 #include <windows.h>
 
